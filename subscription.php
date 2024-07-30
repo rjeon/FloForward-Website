9272571cbdcf2df5b118c90e46f67fa9e7ca73ae
rjeon <rjeon3@gmail.com>
2024-07-30 01:28:42 -0400

    Updates to Form
    Made changes to form and added icons

---
+++ subscription.php
@@ -0,0 +1,32 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Collect form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+
+// Insert data into database
+$sql = "INSERT INTO users (name, email, comment) VALUES ('$name', '$email', '$comment')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
